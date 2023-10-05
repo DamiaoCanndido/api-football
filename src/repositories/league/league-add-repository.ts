cbcdd7deba6a2b837514e8480b4cba73cfc29f41
DamiaoCanndido <tower_gamer@hotmail.com>
2023-10-05 07:03:35 -0300

    Validate country id exists on league creation

--- src/repositories/league/league-add-repository.ts
+++ src/repositories/league/league-add-repository.ts
@@ -1,20 +1,35 @@
 import { prisma } from '../../infra';
 import { League } from '../../entities';
 import { LeagueAddInterface } from '../../interfaces/league';
+import { HttpException } from '../../errors';
 
 export class LeagueAddRepository implements LeagueAddInterface {
   async add(lgParam: League): Promise<League> {
-    const league = await prisma.league.create({
-      data: {
-        name: lgParam.name,
-        logo: lgParam.logo,
-        season: lgParam.season,
-        type: lgParam.type,
-        numberOfRounds: lgParam.numberOfRounds,
-        rounds: lgParam.rounds,
-        countryId: lgParam.countryId,
-      },
-    });
-    return league;
+    try {
+      const countryExists = await prisma.team.findUnique({
+        where: {
+          id: lgParam.countryId!,
+        },
+      });
+
+      if (!countryExists) {
+        throw new HttpException(404, 'country id not found.');
+      }
+
+      const league = await prisma.league.create({
+        data: {
+          name: lgParam.name,
+          logo: lgParam.logo,
+          season: lgParam.season,
+          type: lgParam.type,
+          numberOfRounds: lgParam.numberOfRounds,
+          rounds: lgParam.rounds,
+          countryId: lgParam.countryId,
+        },
+      });
+      return league;
+    } catch (error) {
+      throw new HttpException(404, 'country id not valid.');
+    }
   }
 }
